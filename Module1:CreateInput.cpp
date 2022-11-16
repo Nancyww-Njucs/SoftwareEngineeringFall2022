2f7e185061655c1476eae6fc2a67dec32e990c01
Nancyww-Njucs <2214382590@qq.com>
2022-11-16 19:41:03 +0800

    6

--- Module1:CreateInput.cpp
+++ Module1:CreateInput.cpp
@@ -18,7 +18,7 @@ using namespace std;
 int CreateRandomInput(char* directory) 
 {                
     std::string format_path = std::string(directory) + std::string("/stdin_format.txt");
-    //------------------------------------------------------------------------------
+    //------------------------以string形式读取stdin_format.txt-------------------
     FILE *fp;
 
     if((fp = fopen(format_path.c_str(),"r")) == 0)
@@ -38,12 +38,10 @@ int CreateRandomInput(char* directory)
         if(fgets(szTest,sizeof(szTest),fp)==0)
         {
             break;
-        }
-        //printf("%s\n", szTest);                        
+        }                    
     }
     fclose(fp);
-    //printf("%s", szTest);
-    //printf("%d\n",count);
+
     int begin = 0,end = 0;
     for(int i = 0; szTest[i] != '\0'; i++)
     {
@@ -64,7 +62,6 @@ int CreateRandomInput(char* directory)
     return 0;
 }
 
-
 //------------------------Create random input ----> txt----------------------------
 void HandleChar(char* arr, int begin, int end)
 {
@@ -73,12 +70,9 @@ void HandleChar(char* arr, int begin, int end)
     if((fp = fopen("./input.txt","a"))==0)
     {
         printf("文件打开失败!\n");
-        //return -1;
     }
-
     if(arr[begin] == 'i')
     {
-        //random
         int low = 0;
         int high = 0;
         int i = begin + 4;
@@ -88,14 +82,11 @@ void HandleChar(char* arr, int begin, int end)
         for(int j = i+1;arr[j]!=')';j++)
             high = (arr[j] - '0') + high*10;
         int random = low+rand()%(high-low+1); 
-        //printf("%d ",random);
         fprintf(fp,"%d",random);
-        //write down       
     }
     else if(arr[begin] == 'c')
     {
         int random = 97+rand()%(122-97+1); 
-        //printf("%c ",random+' '-32);
         fprintf(fp,"%c",random+' '-32);
     }
     else if(arr[begin] == 's')
@@ -112,34 +103,15 @@ void HandleChar(char* arr, int begin, int end)
         int random_length = low+rand()%(high-low+1);
         for(int k = 1;k <= random_length;k++)
         {
-            //printf("%c",48+rand()%(122-48+1)+' '-32);
             fprintf(fp,"%c",48+rand()%(122-48+1)+' '-32);
         }
-        //printf(" ");
     }
     fprintf(fp,"%c",32);
     fclose(fp);
 }
 
-
 void showAllFiles(const char *dir_name, std::vector<std::string>& filesAbsolutePath)
 {
-	// check the parameter !
-	if (NULL == dir_name)
-	{
-		cout << " dir_name is null ! " << endl;
-		return;
-	}
-
-	// check if dir_name is a valid dir
-	struct stat s = {};
-	lstat(dir_name, &s);
-	if (!S_ISDIR(s.st_mode))
-	{
-		cout << "dir_name is not a valid directory !" << endl;
-		return;
-	}
-
 	struct dirent *filename = NULL;
 	DIR* dir = opendir(dir_name);
 	if (NULL == dir)
@@ -149,22 +121,18 @@ void showAllFiles(const char *dir_name, std::vector<std::string>& filesAbsoluteP
 	}
 	cout << "Successfully opened the dir !" << endl;
 
-	/* read all the files in the dir ~ */
 	while ((filename = readdir(dir)) != NULL)
 	{
-		// get rid of "." and ".."
 		if (strcmp(filename->d_name, ".") == 0 ||
 			strcmp(filename->d_name, "..") == 0)
 			continue;
-		//cout<<"FileName:"<<filename->d_name << endl;
-		//
+
         char name[1000] = {0};
         strncpy(name, filename->d_name, strlen(filename->d_name)-4);
         if(strcmp(name, "stdin_format") != 0 && strcmp(name, "input") != 0)
 		{
             //cout<<"FileName:"<<name << endl;
             //cout<<"FilePath:"<<dir_name<<"/"<<filename->d_name << endl;
-            
             std::string absolutePath = dir_name + std::string("/") + filename->d_name;
             filesAbsolutePath.push_back(absolutePath);
         }
